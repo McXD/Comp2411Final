bc9babf6de4e382b8fcd787c06a18c4bc224e7cb
McXD <74167137+McXD@users.noreply.github.com>
2020-12-08 16:35:03 +0800

    organize

---
+++ Comp2411Project_V1.1/src/Application.java
@@ -0,0 +1,19 @@
+import java.awt.EventQueue;
+
+import gui.MainGUI;
+
+public class Application {
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					MainGUI frame = new MainGUI();
+					frame.setLocationRelativeTo(null);
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+}
--- Comp2411Project_V1.1/src/admin/Administration.java
+++ Comp2411Project_V1.1/src/admin/Administration.java
@@ -1,3 +1,4 @@
+package admin;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileWriter;
